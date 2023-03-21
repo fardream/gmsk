03be0951ce9cce5da73b392ff2a2915b7019838d
Chao Xu <fardream@users.noreply.github.com>
2023-03-21 18:22:55 -0400

    basic portfolio example (#12)

--- cmosek.go
+++ cmosek.go
@@ -19,108 +19,6 @@ import (
 	"unsafe"
 )
 
-// INFINITY is MSK_INFINITY (which is different from the double's infinity)
-const INFINITY = C.MSK_INFINITY
-
-// Int32t is the int type in MOSEK, which is int32_t/int32
-type Int32t = C.MSKint32t
-
-// Int64t is the 64 bit integer in MOSEK, which is int64_t/int64
-type Int64t = C.MSKint64t
-
-// Realt is the double type in MOSEK, which is double/float64
-type Realt = C.MSKrealt
-
-// ResCode is the enumerator of MSKrescodee
-type ResCode = C.MSKrescodee
-
-const (
-	RES_OK        ResCode = C.MSK_RES_OK        // OK return code from mosek functions
-	RES_ERR_SPACE ResCode = C.MSK_RES_ERR_SPACE // Err
-)
-
-// MSK_MAX_STR_LEN is the max length of strings in mosek
-const MAX_STR_LEN = C.MSK_MAX_STR_LEN
-
-// ObjectiveSense is the MSKobjsense type
-type ObjectiveSense = C.MSKobjsensee
-
-const (
-	OBJECTIVE_SENSE_MINIMIZE ObjectiveSense = C.MSK_OBJECTIVE_SENSE_MINIMIZE // Objective is to maximize
-	OBJECTIVE_SENSE_MAXIMIZE ObjectiveSense = C.MSK_OBJECTIVE_SENSE_MAXIMIZE // Objective is to minimize
-)
-
-// SolType is the solution type
-type SolType = C.MSKsoltypee
-
-const (
-	SOL_ITR SolType = C.MSK_SOL_ITR // Iterior Point Solution.
-	SOL_BAS SolType = C.MSK_SOL_BAS // Basic Solution.
-	SOL_ITG SolType = C.MSK_SOL_ITG // Integer Solution.
-)
-
-// SolSta is the solution status
-type SolSta = C.MSKsolstae
-
-const (
-	SOL_STA_UNKNOWN            SolSta = C.MSK_SOL_STA_UNKNOWN
-	SOL_STA_OPTIMAL            SolSta = C.MSK_SOL_STA_OPTIMAL
-	SOL_STA_PRIM_FEAS          SolSta = C.MSK_SOL_STA_PRIM_FEAS
-	SOL_STA_DUAL_FEAS          SolSta = C.MSK_SOL_STA_DUAL_FEAS
-	SOL_STA_PRIM_AND_DUAL_FEAS SolSta = C.MSK_SOL_STA_PRIM_AND_DUAL_FEAS
-	SOL_STA_PRIM_INFEAS_CER    SolSta = C.MSK_SOL_STA_PRIM_INFEAS_CER
-	SOL_STA_DUAL_INFEAS_CER    SolSta = C.MSK_SOL_STA_DUAL_INFEAS_CER
-	SOL_STA_PRIM_ILLPOSED_CER  SolSta = C.MSK_SOL_STA_PRIM_ILLPOSED_CER
-	SOL_STA_DUAL_ILLPOSED_CER  SolSta = C.MSK_SOL_STA_DUAL_ILLPOSED_CER
-	SOL_STA_INTEGER_OPTIMAL    SolSta = C.MSK_SOL_STA_INTEGER_OPTIMAL
-)
-
-// BoundKey is MSKboundkey enum, indicate the type of the bound
-type BoundKey = C.MSKboundkeye
-
-const (
-	BK_LO BoundKey = C.MSK_BK_LO // Lower bound
-	BK_UP BoundKey = C.MSK_BK_UP // Upper bound
-	BK_FX BoundKey = C.MSK_BK_FX // Fixed bound
-	BK_FR BoundKey = C.MSK_BK_FR // Free
-	BK_RA BoundKey = C.MSK_BK_RA // Range bound
-)
-
-// StreamType is MSKstreamtypee, the type of the stream.
-type StreamType = C.MSKstreamtypee
-
-const (
-	STREAM_LOG StreamType = C.MSK_STREAM_LOG
-	STREAM_MSG StreamType = C.MSK_STREAM_MSG
-	STREAM_ERR StreamType = C.MSK_STREAM_ERR
-	STREAM_WRN StreamType = C.MSK_STREAM_WRN
-)
-
-// DataFormat is MSKdataformate and format of the data file.
-type DataFormat = C.MSKdataformate
-
-const (
-	DATA_FORMAT_EXTENSION DataFormat = C.MSK_DATA_FORMAT_EXTENSION
-	DATA_FORMAT_MPS       DataFormat = C.MSK_DATA_FORMAT_MPS
-	DATA_FORMAT_LP        DataFormat = C.MSK_DATA_FORMAT_LP
-	DATA_FORMAT_OP        DataFormat = C.MSK_DATA_FORMAT_OP
-	DATA_FORMAT_FREE_MPS  DataFormat = C.MSK_DATA_FORMAT_FREE_MPS
-	DATA_FORMAT_TASK      DataFormat = C.MSK_DATA_FORMAT_TASK
-	DATA_FORMAT_PTF       DataFormat = C.MSK_DATA_FORMAT_PTF
-	DATA_FORMAT_CB        DataFormat = C.MSK_DATA_FORMAT_CB
-	DATA_FORMAT_JSON_TASK DataFormat = C.MSK_DATA_FORMAT_JSON_TASK
-)
-
-// CompressType is the compression type for data file
-type CompressType = C.MSKcompresstypee
-
-const (
-	COMPRESS_NONE CompressType = C.MSK_COMPRESS_NONE
-	COMPRESS_FREE CompressType = C.MSK_COMPRESS_FREE
-	COMPRESS_GZIP CompressType = C.MSK_COMPRESS_GZIP
-	COMPRESS_ZSTD CompressType = C.MSK_COMPRESS_ZSTD
-)
-
 // Env wraps mosek environment
 type Env struct {
 	env C.MSKenv_t
@@ -285,6 +183,11 @@ func (task *Task) PutAfeFEntryList(numentr Int64t, afeidx *Int64t, varidx *Int32
 	return C.MSK_putafefentrylist(task.task, numentr, afeidx, varidx, val)
 }
 
+// PutAfeFRow wraps MSK_putafefrow and sets a row of affine expression F matrix
+func (task *Task) PutAfeFRow(afeidx Int64t, numnz Int32t, varidx *Int32t, val *Realt) ResCode {
+	return C.MSK_putafefrow(task.task, afeidx, numnz, varidx, val)
+}
+
 // PutAfeG wraps MSK_putafeg and sets the value at afeidx to g
 func (task *Task) PutAfeG(afeidx Int64t, g Realt) ResCode {
 	return C.MSK_putafeg(task.task, afeidx, g)
@@ -323,14 +226,43 @@ func (task *Task) AppendRQuadraticConeDomain(n Int64t) (r ResCode, domidx Int64t
 	return
 }
 
-// AppendAcc wraps MSK_appendacc and adds an affine conic constraint to the task.
+// AppendAcc wraps MSK_appendacc and adds an affine conic constraint to the task, where the afe idx is provided
+// by an array or pointer - if the afe idx is sequential, use [AppendAccSeq] to avoid allocating an array.
 func (task *Task) AppendAcc(domidx, numafeidx Int64t, afeidxlist *Int64t, b *Realt) ResCode {
 	return C.MSK_appendacc(task.task, domidx, numafeidx, afeidxlist, b)
+}
+
+// AppendAccSeq wraps MSK_appendaccseq and adds an affine conic constraint to the task where
+// the affine idx is sequential.
+func (task *Task) AppendAccSeq(domidx, numafeidx, afeidxfirst Int64t, b *Realt) ResCode {
+	return C.MSK_appendaccseq(task.task, domidx, numafeidx, afeidxfirst, b)
 }
 
 // AppendAccsSeq wraps MSK_appendaccsseq and append a block of accs to the tas - assuming affine expressions are sequential.
 func (task *Task) AppendAccsSeq(numaccs Int64t, domidxs *Int64t, numafeidx, afeidxfirst Int64t, b *Realt) ResCode {
 	return C.MSK_appendaccsseq(task.task, numaccs, domidxs, numafeidx, afeidxfirst, b)
+}
+
+// PutVarName wraps MSK_putvarname and sets a name for variable at j.
+// Allocate a new C array and copy the data over, then free it - this is a costly function.
+func (task *Task) PutVarName(j Int32t, name string) ResCode {
+	cstr := C.CString(name)
+	defer C.free(unsafe.Pointer(cstr))
+	return C.MSK_putvarname(task.task, j, cstr)
+}
+
+// PutConName wraps MSK_putconname and sets a name for a linear constraint at indext i.
+func (task *Task) PutConName(i Int32t, name string) ResCode {
+	cstr := C.CString(name)
+	defer C.free(unsafe.Pointer(cstr))
+	return C.MSK_putconname(task.task, i, cstr)
+}
+
+// PutAccName wraps MSK_putaccname and sets a name for an affine conic constraint.
+func (task *Task) PutAccName(accidx Int64t, name string) ResCode {
+	cstr := C.CString(name)
+	defer C.free(unsafe.Pointer(cstr))
+	return C.MSK_putaccname(task.task, accidx, cstr)
 }
 
 // OptimizeTerm wraps MSK_optimizeterm, which optimizes the problem.
@@ -351,10 +283,10 @@ func (task *Task) GetSolSta(whichsol SolType) (res ResCode, solSta SolSta) {
 	return
 }
 
-// getXx wraps MSK_getxx, which gets the solution from the task.
+// GetXx wraps MSK_getxx, which gets the solution from the task.
 // xx can be nil, in which case the number of variables will be queried from the task and
 // a new slice created.
-func (task *Task) GetXx(solType SolType, xx []Realt) (ResCode, []Realt) {
+func (task *Task) GetXx(whichsol SolType, xx []Realt) (ResCode, []Realt) {
 	var res ResCode
 	var numVar Int32t
 	if xx == nil {
@@ -365,7 +297,20 @@ func (task *Task) GetXx(solType SolType, xx []Realt) (ResCode, []Realt) {
 		xx = make([]Realt, numVar)
 	}
 
-	res = C.MSK_getxx(task.task, solType, &xx[0])
+	res = C.MSK_getxx(task.task, whichsol, &xx[0])
+
+	return res, xx
+}
+
+// GetXxSlice wraps MSK_getxxslice, which gets a slice of the solution. xx can be nil, in which case the number of variables
+// will be last - first and a new slice will be created.
+func (task *Task) GetXxSlice(whichsol SolType, first, last Int32t, xx []Realt) (ResCode, []Realt) {
+	var res ResCode
+	if xx == nil {
+		xx = make([]Realt, last-first)
+	}
+
+	res = C.MSK_getxxslice(task.task, whichsol, first, last, &xx[0])
 
 	return res, xx
 }
@@ -413,6 +358,11 @@ func (task *Task) EvaluateAcc(whichsol SolType, accidx Int64t, activity []Realt)
 	res = C.MSK_evaluateacc(task.task, whichsol, accidx, &activity[0])
 
 	return res, activity
+}
+
+// PutIntParam wraps MSK_putintparam and sets the param to parvalue.
+func (task *Task) PutIntParam(param IParam, parvalue Int32t) ResCode {
+	return C.MSK_putintparam(task.task, param, parvalue)
 }
 
 // writerHolder holds a writer. This must be passed to C api with a handle.
---
+++ example_portfolio_1_basic_test.go
@@ -0,0 +1,136 @@
+package gmsk_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fardream/gmsk"
+)
+
+func Example_portfolio1Basic() {
+	checkOk := func(r gmsk.ResCode) {
+		if r != gmsk.RES_OK {
+			_, sym, desc := gmsk.GetCodeDescSimple(r)
+
+			log.Fatalf("failed: %s %s", sym, desc)
+		}
+	}
+
+	const (
+		n     gmsk.Int32t = 8
+		gamma gmsk.Realt  = 36
+	)
+	mu := []gmsk.Realt{0.07197349, 0.15518171, 0.17535435, 0.0898094, 0.42895777, 0.39291844, 0.32170722, 0.18378628}
+	// GT must have size n rows
+	GT := [...][8]gmsk.Realt{
+		{0.30758, 0.12146, 0.11341, 0.11327, 0.17625, 0.11973, 0.10435, 0.10638},
+		{0.00000, 0.25042, 0.09946, 0.09164, 0.06692, 0.08706, 0.09173, 0.08506},
+		{0.00000, 0.00000, 0.19914, 0.05867, 0.06453, 0.07367, 0.06468, 0.01914},
+		{0.00000, 0.00000, 0.00000, 0.20876, 0.04933, 0.03651, 0.09381, 0.07742},
+		{0.00000, 0.00000, 0.00000, 0.00000, 0.36096, 0.12574, 0.10157, 0.0571},
+		{0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.21552, 0.05663, 0.06187},
+		{0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.22514, 0.03327},
+		{0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.00000, 0.2202},
+	}
+
+	const k gmsk.Int64t = 8 // this is const MSKint32t k       = sizeof(GT) / (n * sizeof(MSKrealt));
+	x0 := []gmsk.Realt{8.0, 5.0, 3.0, 5.0, 2.0, 9.0, 3.0, 6.0}
+	const w gmsk.Realt = 59
+
+	// Offset of variables into the API variable.
+	var numvar gmsk.Int32t = n
+	var voff_x gmsk.Int32t = 0
+
+	// Constraints offsets
+	var numcon gmsk.Int32t = 1
+	var coff_bud gmsk.Int32t = 0
+
+	env, err := gmsk.MakeEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer gmsk.DeleteEnv(env)
+
+	task, err := gmsk.MakeTask(env, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer gmsk.DeleteTask(task)
+
+	checkOk(task.LinkFuncToTaskStream(gmsk.STREAM_LOG, os.Stderr))
+
+	// Holding variable x of length n
+	// No other auxiliary variables are needed in this formulation
+	checkOk(task.AppendVars(numvar))
+	// Setting up variable x
+	for j := gmsk.Int32t(0); j < n; j++ {
+		/* Optionally we can give the variables names */
+		checkOk(task.PutVarName(voff_x+j, fmt.Sprintf("x[%d]", 1+j)))
+		/* No short-selling - x^l = 0, x^u = inf */
+		checkOk(task.PutVarbound(voff_x+j, gmsk.BK_LO, 0, gmsk.INFINITY))
+	}
+	// One linear constraint: total budget
+	checkOk(task.AppendCons(numcon))
+	checkOk(task.PutConName(coff_bud, "budget"))
+	for j := gmsk.Int32t(0); j < n; j++ {
+		/* Coefficients in the first row of A */
+		checkOk(task.PutAij(coff_bud, voff_x+j, 1))
+	}
+	totalBudget := w
+	for _, ax0 := range x0 {
+		totalBudget += ax0
+	}
+	checkOk(task.PutConBound(coff_bud, gmsk.BK_FX, totalBudget, totalBudget))
+
+	// Input (gamma, GTx) in the AFE (affine expression) storage
+	// We need k+1 rows
+	checkOk(task.AppendAfes(k + 1))
+	// The first affine expression = gamma
+	checkOk(task.PutAfeG(0, gamma))
+	// The remaining k expressions comprise GT*x, we add them row by row
+	// In more realisic scenarios it would be better to extract nonzeros and input in sparse form
+	vslice_x := make([]gmsk.Int32t, n)
+	for i := gmsk.Int32t(0); i < n; i++ {
+		vslice_x[i] = voff_x + i
+	}
+	for i := gmsk.Int64t(0); i < k; i++ {
+		checkOk(task.PutAfeFRow(i+1, n, &vslice_x[0], &GT[i][0]))
+	}
+
+	// Input the affine conic constraint (gamma, GT*x) \in QCone
+	// Add the quadratic domain of dimension k+1
+	r, qdom := task.AppendQuadraticConeDomain(k + 1)
+	checkOk(r)
+	// Add the constraint
+	checkOk(task.AppendAccSeq(qdom, k+1, 0, nil))
+	checkOk(task.PutAccName(0, "risk"))
+
+	// Objective: maximize expected return mu^T x
+	for j := gmsk.Int32t(0); j < n; j++ {
+		checkOk(task.PutCj(voff_x+j, mu[j]))
+	}
+	checkOk(task.PutObjsense(gmsk.OBJECTIVE_SENSE_MAXIMIZE))
+
+	/* No log output */
+	checkOk(task.PutIntParam(gmsk.IPAR_LOG, 0))
+
+	/* Dump the problem to a human readable PTF file. */
+	checkOk(task.WriteDataHandle(os.Stderr, gmsk.DATA_FORMAT_PTF, gmsk.COMPRESS_NONE)) // dump to stderr instead.
+
+	r, _ = task.OptimizeTerm()
+	checkOk(r)
+
+	var expret gmsk.Realt
+
+	for j := gmsk.Int32t(0); j < n; j++ {
+		r, xx := task.GetXxSlice(gmsk.SOL_ITR, voff_x+j, voff_x+j+1, nil)
+		checkOk(r)
+		xj := xx[0]
+		expret += mu[j] * xj
+	}
+
+	/* Read the value of s. This should be gamma. */
+	fmt.Printf("\nExpected return %e for gamma %e\n", expret, gamma)
+	// Output: Expected return 4.192247e+01 for gamma 3.600000e+01
+}
