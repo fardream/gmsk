b6112716d075541053e797399bea2e02432c61dc
Chao Xu <fardream@users.noreply.github.com>
2023-04-15 19:07:01 -0400

    add `String()` to enums (#46)

--- res/code.go
+++ res/code.go
@@ -5,9 +5,6 @@
 // [response code]: https://docs.mosek.com/latest/capi/response-codes.html
 package res
 
-// Code is the return code from MOSEK routines
-type Code uint32
-
 // IsOk checks if the result is ok.
 func (c Code) IsOk() bool {
 	return c == OK
@@ -16,9 +13,4 @@ func (c Code) IsOk() bool {
 // NotOk checks if the result is not ok.
 func (c Code) NotOk() bool {
 	return c != OK
-}
-
-func (c Code) GetMsg() (string, bool) {
-	s, f := resCodeMsg[c]
-	return s, f
 }
